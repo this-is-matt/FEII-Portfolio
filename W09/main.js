04e624bd3b40cd61377d5561362d892c69a813d3
this-is-matt <mcp6000@gmail.com>
2022-03-02 18:04:45 -0700

    W09

---
+++ W09/main.js
@@ -0,0 +1,18 @@
+// const squareElement = document.getElementById('square');
+// let angle = 0;
+
+// setInterval( () => {
+//     angle = (angle + 2) % 360;
+//     squareElement.style.transform = `rotate(${angle}deg)`
+// }, 1000/60);
+
+const squareElement = document.getElementById('square');
+let angle = 0;
+
+function rotate() {
+    angle = (angle + 2)%360;
+    squareElement.style.transform = `rotate(${angle}deg)`
+    window.requestAnimationFrame(rotate);
+}
+
+const id = requestAnimationFrame(rotate);
